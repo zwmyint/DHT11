776086c15903a55c287d423dd41200b78198bf73
Codnpix <pierrejarriges@gmail.com>
2018-11-30 14:05:40 +0100

    init exo

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href='' rel='stylesheet' type='text/css'>
+        <title></title>
+    </head>
+    <body>
+        <h1>Hello world</h1>
+    </body>
+</html>
