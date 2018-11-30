bdd89376d4e8c5146b6ca17bcd68b9d95157f641
Codnpix <pierrejarriges@gmail.com>
2018-11-30 14:37:22 +0100

    mise en place image thermo

--- index.php
+++ index.php
@@ -3,9 +3,29 @@
     <head>
         <meta charset="utf-8">
         <link href='' rel='stylesheet' type='text/css'>
-        <title></title>
+        <title>Exo affichage température</title>
+        <style>
+            #mercure {
+                position: absolute;
+                background-color: red;
+                width: 8px;
+                height: 10px;
+                bottom: 85px;
+                left: 44px;
+            }
+            #thermo {
+                position: relative;
+            }
+        </style>
     </head>
     <body>
-        <h1>Hello world</h1>
+        <h1>Température</h1>
+        <p id="infoMsg">Il fait <span id="tmp"></span>°C avec <span id="wet"></span>% d'humidité.<br>
+        Le <span id="date"></span> à <span id = "hour"></span></p>
+        <div id="thermo">
+            <img src="thermo.jpg" height=400>
+            <div id="mercure"></div>
+        </div>
+        
     </body>
 </html>
