cd5da913d7c0df5523c3db610b2482603f3bf426
Codnpix <pierrejarriges@gmail.com>
2018-12-21 11:10:41 +0100

    try send json nodemcu

--- store.php
+++ store.php
@@ -1,15 +1,15 @@
 <?php
-if (isset($_POST['temperature']) && isset($POST['humidite'])) {
-  $new_tmp = $_POST['temperature'];
-  $new_hum = $_POST['humidite'];
-  $json_data = '{"temperature":'.$new_tmp .','.'"humidite":'.$new_hum.'}';
-}
+// if (isset($_POST['temperature']) && isset($POST['humidite'])) {
+//   $new_tmp = $_POST['temperature'];
+//   $new_hum = $_POST['humidite'];
+//   $json_data = '{"temperature":'.$new_tmp .','.'"humidite":'.$new_hum.'}';
+// }
 
 $myFile = 'data.txt';
-// $new_json = file_get_contents("php://input");
+$new_json = file_get_contents("php://input");
 //
 // verif donnée - renvoi des erreurs possibles
-$data = json_decode($json_data);
+$data = json_decode($new_json);
 if (!$data) {
     http_response_code(415);
     exit();
@@ -17,9 +17,9 @@ if (!$data) {
     http_response_code(400);
     exit();
 }
-file_put_contents($myFile, $json_data);
+file_put_contents($myFile, $new_json);
 
-if (!$json_data) {
+if (!$new_json) {
     http_response_code(500);
     exit();
 }
