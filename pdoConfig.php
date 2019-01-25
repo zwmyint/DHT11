852c57b231183a3e9ca376aab57bfd9bfeb159ae
Codnpix <pierrejarriges@gmail.com>
2019-01-25 10:58:07 +0100

    gitignore txt

--- pdoConfig.php
+++ pdoConfig.php
@@ -2,5 +2,5 @@
 $host = 'localhost'; // Ou Adresse IP du server 11.22.33.44
 //$dbname = 'DHT11_db';
 $username = 'root';
-//$password = '52dLmenJspp@y!?ueiHHt';//password vps
-$password = 'Peterclash@072642';//password localhost Pierre
+$password = '52dLmenJspp@y!?ueiHHt';//password vps
+//$password = 'Peterclash@072642';//password localhost Pierre
